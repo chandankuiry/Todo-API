f06fe056e1bdb56cbe78ff1e1e49db5ef548eaa9
chandan kuiry <kuirychandan@gmail.com>
2015-12-17 04:47:47 +0530

    Add todo model and POST todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db=require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -56,7 +57,14 @@ app.get('/todos/:id', function(req, res) {
 // POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	//isBoolean,isString is builtin function of underscore library .trim() is used for avoid space
+
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
+		res.status(400).json(e);
+		
+	});
+	/*//isBoolean,isString is builtin function of underscore library .trim() is used for avoid space
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
@@ -68,7 +76,7 @@ app.post('/todos', function(req, res) {
 	// push body into array
 	todos.push(body);
 
-	res.json(body);
+	res.json(body);*/
 });
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
@@ -111,6 +119,9 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 
 });
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function (){
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
+	
 });
