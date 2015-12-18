f3a86b0b022d16169de4d7f34bc8427603bc561a
chandan kuiry <kuirychandan@gmail.com>
2015-12-18 21:32:58 +0530

    convert email to lowercase

--- models/user.js
+++ models/user.js
@@ -15,8 +15,17 @@ module.exports=function (sequelize, DataTypes) {
 			validate: {
 				len:[7, 100]//password should be 7-100 letter
 			}
-		}		
+		}
+	}, {
+		hooks: {
+			beforeValidate: function (user, options){
+				if (typeof user.email === 'string') {
+					user.email=user.email.toLowerCase();
+				}
+			}
+		}
+				
 
 	});	
 
-}		
+};		
