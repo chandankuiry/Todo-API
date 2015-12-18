d99cb45f28b569c72b1b84b76691ceda4303d3bb
chandan kuiry <kuirychandan@gmail.com>
2015-12-19 04:33:39 +0530

    add authenticate class in user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var bcrypt =require('bcrypt');
 var _=require('underscore');
 module.exports=function (sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user =sequelize.define('user', {
 		email: {
 			type:DataTypes.STRING,
 			allowNull:false,
@@ -42,6 +42,34 @@ module.exports=function (sequelize, DataTypes) {
 				}
 			}
 		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							//compareSync check if body.password ,password_hash (look into user.js) are same or not
+							return reject();
+						}
+
+
+						resolve(user); //if we set it toJSON then it also show the salt and password_hash
+					}, function(e) {
+						reject();
+
+
+					});
+
+				});
+			}
+
+		},
 		// to secure our password more efficiently that we don't want to return our password 
 		instanceMethods: {
 			toPublicJSON: function(){
@@ -53,6 +81,7 @@ module.exports=function (sequelize, DataTypes) {
 		}
 				
 
-	});	
+	});
+	return user;	
 
 };		
