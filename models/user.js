de1f0a6ee405ee2c43acbb783fef9b44705341a0
chandan kuiry <kuirychandan@gmail.com>
2015-12-19 00:07:20 +0530

    hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+var bcrypt =require('bcrypt');
+var _=require('underscore');
 module.exports=function (sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
@@ -9,11 +11,27 @@ module.exports=function (sequelize, DataTypes) {
 	
 			}
 		},
-		password: {
+		salt: {
 			type:DataTypes.STRING,
+		},
+		password_hash: {
+			type:DataTypes.STRING
+
+		},
+		password: {
+			type:DataTypes.VIRTUAL,
 			allowNull:false,
 			validate: {
 				len:[7, 100]//password should be 7-100 letter
+			},
+			set: function(value){
+				var salt=bcrypt.genSaltSync(10);//it add salt
+				var hashedPassword=bcrypt.hashSync(value,salt);
+				this.setDataValue('password',value);
+				this.setDataValue('salt',salt);
+				this.setDataValue('password_hash',hashedPassword );
+
+
 			}
 		}
 	}, {
@@ -22,6 +40,15 @@ module.exports=function (sequelize, DataTypes) {
 				if (typeof user.email === 'string') {
 					user.email=user.email.toLowerCase();
 				}
+			}
+		},
+		// to secure our password more efficiently that we don't want to return our password 
+		instanceMethods: {
+			toPublicJSON: function(){
+				var json =this.toJSON();
+				//we want to return id,email,createdAt,updatedAt we don't want to print password,password_hash,salt
+				return _.pick(json,'id','email','createdAt','updatedAt');
+
 			}
 		}
 				
