023127312211083ac823b2d31575aaa5f2493a1b
chandan kuiry <kuirychandan@gmail.com>
2015-12-19 06:43:34 +0530

    add auth token generation to POST/users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt =require('bcrypt');
 var _=require('underscore');
+var cryptojs=require('crypto-js');
+var jwt=require('jsonwebtoken');
 module.exports=function (sequelize, DataTypes) {
 	var user =sequelize.define('user', {
 		email: {
@@ -77,6 +79,24 @@ module.exports=function (sequelize, DataTypes) {
 				//we want to return id,email,createdAt,updatedAt we don't want to print password,password_hash,salt
 				return _.pick(json,'id','email','createdAt','updatedAt');
 
+			},
+			generateToken:function (type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+				try {
+					var stringData=JSON.stringify({id: this.get('id'),type:type});
+					var encryptedData=cryptojs.AES.encrypt(stringData,'abc123!@#!').toString();//toString return our encrypted data
+					var token=jwt.sign({
+						token:encryptedData
+					}, 'qwerty098');
+
+					return token;
+
+				} catch(e){
+					console.error(e);
+					return undefined;
+				}
 			}
 		}
 				
--- server.js
+++ server.js
@@ -208,9 +208,17 @@ app.post('/users',function (req,res) {
 //POST/users/login
 app.post('/users/login', function (req,res) {
 	var body =_.pick(req.body,'email','password');
+
 	db.user.authenticate(body).then(function (user) {
-		res.json(user.toPublicJSON());
-	}, function (e) {
+		var token=user.generateToken('authentication');
+		if (token){
+			res.header('Auth',token).json(user.toPublicJSON());
+		} else{
+			res.status(401).send();
+
+		}
+		
+	}, function () {
 		res.status(401).send();
 	});
 
@@ -219,7 +227,7 @@ app.post('/users/login', function (req,res) {
 
 
 
-db.sequelize.sync().then(function (){
+db.sequelize.sync({force:true}).then(function (){
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
